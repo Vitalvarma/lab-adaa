f56d2da031afb4b1c80aff1b0c5f5da7480ff1d5
Vitalvarma <157305632+Vitalvarma@users.noreply.github.com>
2025-01-31 14:23:16 +0530

    Update MinHeap.java

--- heaps/MinHeap.java
+++ heaps/MinHeap.java
@@ -4,14 +4,34 @@ public class MinHeap {
     static void insert(int arr[],int n){
         int item=arr[n];
         int i=n;
-        while(i>0){
-            if(item<arr[(i-1)/2]){
-                arr[i]=arr[(i-1)/2];
-                i=(i-1)/2;
-            }
-            arr[i]=item;
+        while(i>0 && item<arr[(i-1)/2]){
+            arr[i]=arr[(i-1)/2];
+            i=(i-1)/2;
         }
+        arr[i]=item;
+    }
 
+    static int delete(int arr[],int n){
+        int item=arr[0];
+        arr[0]=arr[n];
+        arr[n]=item;
+
+        adjust(arr,n-1); 
+        return item;
+    }
+
+    static void adjust(int arr[],int n){
+        int item=arr[0];
+        int i=0;
+        int j=1;
+        while(j<=n){
+            if(j+1<=n && arr[j+1]<arr[j]){j++;}
+            if(item<arr[j]) break;
+            arr[i]=arr[j];
+            i=j;
+            j=2*i+1;
+        }
+        arr[i]=item;
     }
 
     public static void main(String args[]) {
@@ -25,7 +45,6 @@ public class MinHeap {
                 System.out.print(arr[i] + " ");
             }
             System.out.println();
-            /* */
             int temp=delete(arr, n - 1);
             n--;
             System.out.println("Heap after deletion:");
@@ -33,7 +52,7 @@ public class MinHeap {
                 System.out.print(arr[i] + " ");
             }
             System.out.println();
-            System.out.println("Deleted element: "+temp);*/
+            System.out.println("Deleted element: "+temp);
         }
     }
 }
